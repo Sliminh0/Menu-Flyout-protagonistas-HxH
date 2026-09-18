e8d5c437e6648510212d08384874af8e275871ba
Nalugsw <00001128535750sp@al.educacao.sp.gov.br>
2026-09-18 11:49:17 -0300

    Aula 3

--- Menu Flyout protagonistas HxH/appFlyoutpage.xaml.cs
+++ Menu Flyout protagonistas HxH/appFlyoutpage.xaml.cs
@@ -1,6 +1,6 @@
 namespace Menu_Flyout_protagonistas_HxH;
 
-public partial class appFlyoutpage : ContentPage
+public partial class appFlyoutpage : FlyoutPage
 {
 	public appFlyoutpage()
 	{
@@ -8,4 +8,34 @@ public partial class appFlyoutpage : ContentPage
 
 		Detail = new NavigationPage(new MainPage());
 	}
+
+	private void GonClicked (object? sender, EventArgs e)
+	{
+		Detail = new NavigationPage(new Gon());
+        IsPresented = false;
+	}
+
+    private void KilluaClicked(object? sender, EventArgs e)
+    {
+        Detail = new NavigationPage(new Killua());
+        IsPresented = false;
+    }
+
+    private void KurapikaClicked(object? sender, EventArgs e)
+    {
+        Detail = new NavigationPage(new Kurapika());
+        IsPresented = false;
+    }
+
+    private void LeorioClicked(object? sender, EventArgs e)
+    {
+        Detail = new NavigationPage(new Leorio());
+        IsPresented = false;
+    }
+
+    private void MenuClicked(object? sender, EventArgs e)
+    {
+        Detail = new NavigationPage(new MainPage());
+        IsPresented = false;
+    }
 }
